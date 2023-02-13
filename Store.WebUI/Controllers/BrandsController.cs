9de0099199f54aa92c6f7f921091e9c7b3a1c40a
KubraGavuzoglu <100765765+KubraGavuzoglu@users.noreply.github.com>
2023-02-13 15:38:55 +0300

    13.02 15.38

---
+++ Store.WebUI/Controllers/BrandsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Entities;
+using Store.Service.Abstract;
+using Store.WebUI.Models;
+
+namespace Store.WebUI.Controllers
+{
+    public class BrandsController : Controller
+    {
+        private readonly IService<Brand> _serviceBrand;
+        private readonly IService<Product> _service;
+        public BrandsController(IService<Brand> serviceBrand, IService<Product> service)
+        {
+            _serviceBrand = serviceBrand;
+            _service = service;
+        }
+
+        public async Task<IActionResult> IndexAsync(int id)
+        {
+            var model = new BrandPageViewModel()
+            {
+                Brand = await _serviceBrand.FindAsync(id),
+                Products = await _service.GetAllAsync(p => p.IsActive && p.BrandId == id)
+            };
+            return View(model);
+        }
+    }
+}
